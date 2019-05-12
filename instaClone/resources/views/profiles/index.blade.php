12cb25768709387a540e8126bfd8969d64c44a72
OstapHetman <prickly4528@gmail.com>
2019-05-12 21:21:34 +0300

    Model Policy

--- instaClone/resources/views/profiles/index.blade.php
+++ instaClone/resources/views/profiles/index.blade.php
@@ -9,11 +9,15 @@
       <div class="col-9 pt-5">
         <div class="d-flex justify-content-between align-items-center">
           <h1>{{ $user->username }}</h1>
-          <a href="/p/create" class="btn btn-sm btn-primary">Add New Post</a>
+          @can('update', $user->profile)
+            <a href="/p/create" class="btn btn-sm btn-primary">Add New Post</a>
+          @endcan
         </div>
-        <div class="pb-3">
-            <a href="/profile/{{ $user->id }}/edit" class="btn btn-sm btn-primary">Edit Profile</a>
-        </div>
+        @can('update', $user->profile)
+          <div class="pb-3">
+              <a href="/profile/{{ $user->id }}/edit" class="btn btn-sm btn-primary">Edit Profile</a>
+          </div>
+        @endcan
         <div class="d-flex">
           <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
           <div class="pr-5"><strong>23k</strong> followers</div>
